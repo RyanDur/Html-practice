1b9b40c4f55ff9769f738478096291ab43eb5ad3
RyanDur <ryan.p.durling@gmail.com>
2013-06-21 00:55:10 -0700

    add waypoints to float navigation with scroll

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,8 +1,22 @@
-$(function() {
-    $("nav").css('opacity', 0);
-    $(window).load(revealNav);
-});
-
 var revealNav = function() {
     $("nav").delay(1000).animate({opacity: 1}, 1500);
 };
+
+var stickySection = function(container, section) {
+    container.waypoint({
+        handler: function(direction) {
+                    section.toggleClass('sticky', direction=='down');
+                    if (direction == 'down')
+                       container.css({ 'height': section.outerHeight()  });
+                    else
+                       container.css({ 'height':'auto'  });
+                 }
+    });
+};
+
+$(function() {
+    $("nav").css('opacity', 0);
+    $(window).load(revealNav);
+    stickySection($(".nav-container"), $("nav"));
+});
+
