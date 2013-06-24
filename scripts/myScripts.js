051a7115942fb1e713a0004a2466c7f7471a5999
RyanDur <ryan.p.durling@gmail.com>
2013-06-23 21:24:52 -0700

    side panels move in at same time using css transitions

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,35 +1,40 @@
-var revealNav = function() {
-    $("nav").delay(1000).animate({opacity: 1}, 1500);
+var reveal = function(section) {
+    section.delay(1000).animate({opacity: 1}, 1500);
 };
 
-var stickySection = function(container, section) {
-    var top_spacing = 15;
-    var waypoint_offset = 150;
+var slidePanelsIn = function() {
+    $('.sidebar-left').addClass('slideIn');
+    $('.sidebar-right').addClass('slideIn');
+}
+
+var stickySection = function(container, section, options) {
     container.waypoint({
         handler: function(direction) {
                     if (direction == 'down'){
                         container.css({'height': section.outerHeight()});
-                        section.addClass('sticky')
-                            .stop()
+                        section.stop()
+                            .addClass('sticky')
                             .css('top', -section.outerHeight())
-                            .animate({'top': top_spacing});
+                            .animate({'top': options.top_spacing});
                     } else {
                         container.css({'height':'auto'});
-                        section.removeClass('sticky')
-                            .stop()
-                            .css("top", section.outerHeight() + waypoint_offset)
+                        section.stop()
+                            .removeClass('sticky')
+                            .css("top", section.outerHeight() + options.waypoint_offset)
                             .animate({'top': ""});
                     }
                  },
         offset: function() {
-                    return -(section.outerHeight()+waypoint_offset);
+                    return -(section.outerHeight()+options.waypoint_offset);
                 }
     });
 };
 
 $(function() {
-    $("nav").css('opacity', 0);
-    $(window).load(revealNav);
-    stickySection($(".nav-container"), $("nav"));
+    //$("nav").css('opacity', 0);
+    //reveal($('nav'));
+    var options = {top_spacing: 15, waypoint_offset: 150};
+    stickySection($(".nav-container"), $("nav"), options);
+    slidePanelsIn();
 });
 
