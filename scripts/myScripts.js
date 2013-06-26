81bdc7396c7fa686be79319c3299d70bdf6b89d0
RyanDur <ryan.p.durling@gmail.com>
2013-06-26 00:35:58 -0700

    Ajax call to github to present repos in sidebar left

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -10,19 +10,19 @@ var slidePanelsIn = function() {
 var stickySection = function(container, section, options) {
     container.waypoint({
         handler: function(direction) {
-                    if (direction == 'down'){
-                        container.css({'height': section.outerHeight()});
-                        section.stop()
-                            .addClass('sticky')
-                            .css('top', -section.outerHeight())
-                            .animate({'top': options.top_spacing});
-                    } else {
-                        container.css({'height':'auto'});
-                        section.stop()
-                            .removeClass('sticky')
-                            .css("top", section.outerHeight() + options.waypoint_offset)
-                            .animate({'top': ""});
-                    }
+                     if (direction == 'down'){
+                         container.css({'height': section.outerHeight()});
+                         section.stop()
+        .addClass('sticky')
+        .css('top', -section.outerHeight())
+        .animate({'top': options.top_spacing});
+                     } else {
+                         container.css({'height':'auto'});
+                         section.stop()
+        .removeClass('sticky')
+        .css("top", section.outerHeight() + options.waypoint_offset)
+        .animate({'top': ""});
+                     }
                  },
         offset: function() {
                     return -(section.outerHeight()+options.waypoint_offset);
@@ -30,9 +30,29 @@ var stickySection = function(container, section, options) {
     });
 };
 
+var getRepo = function() {
+    var url = 'https://api.github.com/users/RyanDur/repos'
+        $.ajax({
+            type: 'GET',
+            url: url,
+            cache: false,
+            async: false,
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function(json) {
+                console.dir(json.data[0].name);
+                $.each(json.data, function(index, val) {
+                    $('.git').append("<li><a href="+ val.html_url +
+                    " target=_blank>"+ val.name +"</a></li>");
+                })
+            }
+        });
+};
+
 $(function() {
     var options = {top_spacing: 15, waypoint_offset: 150};
     stickySection($(".nav-container"), $("nav"), options);
     slidePanelsIn();
+    getRepo();
 });
 
