aa5f0d84481b5f0c3979818c95e027bc755446ef
RyanDur <ryan.p.durling@gmail.com>
2013-07-04 21:47:31 -0700

    refactor paginate

--- spec/javascripts/Pagination_Spec.js
+++ spec/javascripts/Pagination_Spec.js
@@ -95,57 +95,37 @@ describe('Pagination', function() {
 
     describe('paginate', function() {
         it('should split up the array into segments according to the size specified', function() {
-            var appendElem1 = function(val) {
-                $('.git1').append("<li><a href="+ val.html_url +">"+ val.name +"</a></li>");
-            };
-            var elem1 = $('<ul class="git1"></ul>');
-            setFixtures(elem1);
-            var page1 = Pagination(appendElem1);
-            page1.paginate(jsonData, 3);
-            expect(page1.total()).toEqual(10);
-            page1.first($('.git1'), $('li'));
-            expect($('ul.git1 > li')).toHaveLength(3);
-            page1.last($('.git1'), $('li'));
-            expect($('ul.git1 > li')).toHaveLength(3);
+            page = Pagination(appendElem);
+            page.paginate(jsonData, 3);
+            expect(page.total()).toEqual(10);
+            page.first($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(3);
+            page.last($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(3);
 
-            var appendElem2 = function(val) {
-                $('.git2').append("<li><a href="+ val.html_url +">"+ val.name +"</a></li>");
-            };
-            var elem2 = $('<ul class="git2"></ul>');
-            setFixtures(elem2);
-            var page2 = Pagination(appendElem2);
-            page2.paginate(jsonData, 5);
-            expect(page2.total()).toEqual(6);
-            page2.first($('.git2'), $('li'));
-            expect($('ul.git2 > li')).toHaveLength(5);
-            page2.last($('.git2'), $('li'));
-            expect($('ul.git2 > li')).toHaveLength(5);
+            page = Pagination(appendElem);
+            page.paginate(jsonData, 5);
+            expect(page.total()).toEqual(6);
+            page.first($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(5);
+            page.last($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(5);
 
-            var appendElem3 = function(val) {
-                $('.git3').append("<li><a href="+ val.html_url +">"+ val.name +"</a></li>");
-            };
-            var elem3 = $('<ul class="git3"></ul>');
-            setFixtures(elem3);
-            var page3 = Pagination(appendElem3);
-            page3.paginate(jsonData, 1);
-            expect(page3.total()).toEqual(30);
-            page3.first($('.git3'), $('li'));
-            expect($('ul.git3 > li')).toHaveLength(1);
-            page3.last($('.git3'), $('li'));
-            expect($('ul.git3 > li')).toHaveLength(1);
+            page = Pagination(appendElem);
+            page.paginate(jsonData, 1);
+            expect(page.total()).toEqual(30);
+            page.first($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(1);
+            page.last($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(1);
 
-            var appendElem4 = function(val) {
-                $('.git4').append("<li><a href="+ val.html_url +">"+ val.name +"</a></li>");
-            };
-            var elem4 = $('<ul class="git4"></ul>');
-            setFixtures(elem4);
-            var page4 = Pagination(appendElem4);
-            page4.paginate(jsonData, 30);
-            expect(page4.total()).toEqual(1);
-            page4.first($('.git4'), $('li'));
-            expect($('ul.git4 > li')).toHaveLength(30);
-            page4.last($('.git4'), $('li'));
-            expect($('ul.git4 > li')).toHaveLength(30);
+            page = Pagination(appendElem);
+            page.paginate(jsonData, 30);
+            expect(page.total()).toEqual(1);
+            page.first($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(30);
+            page.last($('.git'), $('li'));
+            expect($('ul.git > li')).toHaveLength(30);
         })
     });
 });
