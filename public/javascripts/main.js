196ac466c326314d12eb1d233a7510a5b553b51c
RyanDur <ryan.p.durling@gmail.com>
2013-07-05 20:00:45 -0700

    move components back to base directory

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,10 @@
 require.config({
   urlArgs: "bust=" + (new Date()).getTime(), // not to be used in production
+
   paths: {
-          jquery: 'components/jquery/jquery.min',
-          'jqueryui': 'components/jquery-ui/ui/jquery-ui',
-          'waypoints': 'components/jquery-waypoints/waypoints.min',
+          jquery: '../../components/jquery/jquery.min',
+          'jqueryui': '../../components/jquery-ui/ui/minified/jquery-ui.min',
+          'waypoints': '../../components/jquery-waypoints/waypoints.min',
          },
   shim: {
             'jqueryui': {deps: ['jquery']}
