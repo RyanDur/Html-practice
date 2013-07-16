d06153cd3236262f4bf7972c3a5b02c553b572dd
RyanDur <ryan.p.durling@gmail.com>
2013-07-15 17:51:09 -0700

    Pagination menu

--- CowFarmdation/javascripts/PaginationMenu.js
+++ CowFarmdation/javascripts/PaginationMenu.js
@@ -1,13 +1,33 @@
-define(function() {
-  return function() {
-    return {
-      init: function(elem, pageTotal) {
-              elem.find('ul').append('<li class="prev arrow"><a>&laquo;</a></li>');
-              for(var i = 1; i <= pageTotal; i++) {
-                elem.find('ul').append('<li class="page page'+ i +'" data-pagenum="'+ i +'"><a>'+ i +'</a>');
-              }
-              elem.find('ul').append('<li class="next arrow"><a>&raquo;</a></li>');
-            }
+define(['utility'], function(util) {
+  return function(elem, pageTotal) {
+
+      $(elem).find('ul').append('<li class="prev arrow"><a>&laquo;</a></li>');
+      for(var i = 1; i <= pageTotal; i++) {
+          $(elem).find('ul').append('<li class="page page'+ i +'" data-pagenum="'+ i +'"><a>'+ i +'</a>');
+      }
+      $(elem).find('ul').append('<li class="next arrow"><a>&raquo;</a></li>');
+
+      return {
+      current: function(page) {
+                   $(elem).find('li').removeClass('current');
+                   page.addClass('current');
+               },
+
+      unavailable: function(buttons) {
+                       util.forEach(buttons, function(button) {
+                           if (!button.hasClass('unavailable')) {
+                               button.addClass('unavailable');
+                           }
+                       });
+                     },
+
+      available: function(buttons) {
+                     util.forEach(buttons, function(button) {
+                         if (button.hasClass('unavailable')) {
+                             button.removeClass('unavailable');
+                         }
+                     });
+                   }
     };
   };
 });
--- CowFarmdation/spec/javascripts/PaginationMenu_Spec.js
+++ CowFarmdation/spec/javascripts/PaginationMenu_Spec.js
@@ -1,14 +1,73 @@
-define(['PaginationMenu'], function(PaginationMenu) {
-  var menu = PaginationMenu();
+define(['PaginationMenu', 'jasminejquery'], function(PaginationMenu) {
+    var menu;
 
-  beforeEach(function() {
-    setFixtures($('<nav class="repo"><ul></ul></nav>'));
-    menu.init($('nav.repo'), 8);
-  });
+    beforeEach(function() {
+        this.addMatchers({
+            toBeCurrent: function() {
+                             return this.actual.hasClass('current');
+                         },
 
-  describe('init', function() {
-    it('should populate the menu', function() {
-      expect($('ul > li')).toHaveLength(10);
+            toBeAvailable: function() {
+                               return !this.actual.hasClass('unavailable');
+                           }
+        });
+
+        setFixtures($('<nav class="repo"><ul></ul></nav>'));
+        menu = PaginationMenu('nav.repo', 8);
     });
-  });
+
+    describe('init', function() {
+        it('should populate the menu', function() {
+            expect($('ul > li')).toHaveLength(10);
+        });
+    });
+
+    describe('current', function() {
+        it('should make the given element current', function() {
+            menu.current($('.page1'));
+            expect($('.page1')).toBeCurrent();
+
+            menu.current($('.page4'));
+            expect($('.page4')).toBeCurrent();
+
+            menu.current($('.page8'));
+            expect($('.page8')).toBeCurrent();
+        });
+
+        it('should only have one current page', function() {
+            menu.current($('.page2'));
+            expect($('.page2')).toBeCurrent();
+            expect($('ul > li.current')).toHaveLength(1);
+
+            menu.current($('.page5'));
+            expect($('.page5')).toBeCurrent();
+            expect($('ul > li.current')).toHaveLength(1);
+
+            menu.current($('.page7'));
+            expect($('.page7')).toBeCurrent();
+            expect($('ul > li.current')).toHaveLength(1);
+        });
+    });
+
+    describe('unavailable', function() {
+        it('should make the selections unavailable', function() {
+            expect($('.prev')).toBeAvailable();
+            expect($('.page1')).toBeAvailable();
+            menu.unavailable([$('.prev'), $('.page1')]);
+            expect($('.prev')).not.toBeAvailable();
+            expect($('.page1')).not.toBeAvailable();
+        });
+    });
+
+    describe('available', function() {
+        it('should make the selections available', function() {
+            menu.unavailable([$('.next'), $('.page8')]);
+            expect($('.next')).not.toBeAvailable();
+            expect($('.page8')).not.toBeAvailable();
+
+            menu.available([$('.next'), $('.page8')]);
+            expect($('.next')).toBeAvailable();
+            expect($('.page8')).toBeAvailable();
+        });
+    });
 });
